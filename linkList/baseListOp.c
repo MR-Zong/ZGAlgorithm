907648edee80d0aa7a476fd97beac054eb0d72d2
ZongGenXu <540682674@qq.com>
2019-05-17 00:00:08 +0800

    list exchange

--- linkList/baseListOp.c
+++ linkList/baseListOp.c
@@ -6,12 +6,8 @@ typedef struct student{
     struct student *next;
 }studentList;
 
-int * test(int n){
-	printf("n %d",n);
-	return NULL;
-}
 
-studentList * creatList(int n){
+studentList * createList(int n){
    if(n<=0){
 	printf("%d",n);
 	return NULL;
@@ -21,6 +17,7 @@ studentList * creatList(int n){
    end = head;
    for(int i=0;i<n;i++){
 	node = (studentList *)malloc(sizeof(studentList));
+	printf("请输入%d个数字：\n",n);
 	scanf("%d",&node->score);
 	end->next = node; 
 	end = node;
@@ -39,10 +36,67 @@ studentList * getStudent(studentList *head,int n){
 	return node;
 } 
 
+void deleteElement(studentList *head, int n){
+    studentList *preNode, *node = head;
+    int i=0;
+    while(i<n && node!=NULL){
+	if(i==n-1){
+	    preNode = node;
+	}
+	i++;
+	node = node->next;
+    }
+    if(preNode!=NULL&&node!=NULL){
+        preNode->next = node->next;
+        free(node);
+    }
+}  
+
+void printList(studentList *head){
+    studentList *node = head->next;
+    printf("\n list:");
+    while(node!=NULL){
+	printf("score %d,",node->score);
+	node = node->next;
+    }
+    printf("\n");
+}
+
+void exchangeElement(studentList *head, int n1, int n2){
+    studentList *pre1Node, *pre2Node,  *k1Node, *k2Node=head;
+    int maxN = n1>n2?n1:n2;
+    int minN = n1<n2?n1:n2;
+    int i=0;
+    while(i<maxN && k2Node!=NULL){
+	if(i==minN-1){
+	    pre1Node = k2Node;
+	}
+	if(i==minN){
+	    k1Node = k2Node;
+	} 
+	if(i==maxN-1){
+	    pre2Node = k2Node;
+	}
+	i++;
+	k2Node = k2Node->next;
+    }
+    pre1Node->next = k2Node;
+    studentList *k1Next = k1Node->next;
+    k1Node->next = k2Node->next;
+    pre2Node->next = k1Node;
+    k2Node->next = k1Next;
+}
+
 int main()
 {
-    //test(4);
     studentList *list = createList(4);
-    //studentList *node = getStudent(list,3);
-    //printf("score %d",node->score);
+    studentList *node = getStudent(list,3);
+    printf("score %d\n",node->score);
+    printList(list);
+    printf("exchange:\n");
+    exchangeElement(list,1,3);
+    printList(list);
+    printf("delete:\n");
+    deleteElement(list,3);
+    printList(list);
 }
